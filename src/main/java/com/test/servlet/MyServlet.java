45466bf92573f2c68db86ebaadb706ebb60de063
八神 <niceben@yeah.net>
2017-09-05 21:34:39 +0800

    initial commit

---
+++ src/main/java/com/test/servlet/MyServlet.java
@@ -0,0 +1,29 @@
+package com.test.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by 八神 on 2017/9/5.
+ */
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int a = 1;
+        int b = 2;
+
+        int c = a + b;
+
+        System.out.println("doGet invoked");
+        System.out.println("second");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
